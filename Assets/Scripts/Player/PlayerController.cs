22ff521f69cc1a56a2641917343196094a1b74c2
minkalai <renae.reid16@gmail.com>
2026-02-27 21:49:23 -0700

    Startign on Player health

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,15 +5,17 @@ using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] public TextMeshProUGUI coins;
-	[SerializeField] public EnemySpawner reset;
-    public int coinCount;
+    [SerializeField] public TextMeshProUGUI coins, health;
+	//[SerializeField] public EnemySpawner reset;
+    public int coinCount, healthbar, maxHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         coinCount = 0;
         coins.text = coinCount.ToString();
-
+		maxHealth = 100;
+		healthbar = maxHealth;
+		health.text = healthbar.ToString();	
 	}
 
     // Update is called once per frame
